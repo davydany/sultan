680a0c9bacf9cd24d0b283690dcb4c47f0a5e1ff
= <david.daniel@bia-boeing.com>
2016-08-30 13:20:29 -0400

    Bumped up the version to 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='sultan',
     description='Command and Rule over your Shell',
-    version='0.1.3',
+    version='0.1.4',
     author='David Daniel',
     author_email='davydany@aeroxis.com',
     license='MIT',
