e7845790a88456e44b195c0dde1e9d8c764c6ceb
David Daniel <davydany@gmail.com>
2017-09-11 08:22:12 -0400

    Removed 'test_script.py' ... err not sure why it was checked in.

--- test_script.py
+++
@@ -1,8 +0,0 @@
-from sultan.api import Sultan, SSHConfig
-
-config = SSHConfig(port=2222)
-
-with Sultan.load(hostname='localhost', user='vagrant', log=True, ssh_config=config) as sultan:
-    result = sultan.sudo('yum install -y httpd').run()
-    print 'STDOUT: ', '\n'.join(result.stdout)
-    print 'STDERR: ', '\n'.join(result.stderr)
